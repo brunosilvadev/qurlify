66d32e5071439176c178661be265d4d2ab43af59
Bruno Silva <brunosilva.dev@gmail.com>
2024-06-21 16:33:47 -0500

    deleting

--- Data/Shortener.cs
+++
@@ -1,9 +0,0 @@
-namespace qurlify.Data;
-
-public static class Shortener
-{
-    public static string ShortenedUrl (string url)
-    {
-        return url;
-    }
-}
--- Model/Url.cs
+++
@@ -1,9 +0,0 @@
-namespace qurlify.Data; 
-
-public class Url
-{
-    public string OriginalUrl { get; set; } = "";
-    public string ShortenedUrl { get; set; } = "";
-    public string MonthDate { get; set; } = "";
-
-}
