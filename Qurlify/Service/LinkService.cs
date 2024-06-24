745ce3b1c16d9c8fb7fa18497cebabe8d0d40fad
Bruno Silva <brunosilva.dev@gmail.com>
2024-06-24 15:53:10 -0500

    UI updates

--- Qurlify/Service/LinkService.cs
+++ Qurlify/Service/LinkService.cs
@@ -6,9 +6,22 @@ public class LinkService(HttpClient http) : ILinkService
     {
         return await http.GetFromJsonAsync<ShortenedLink[]>("all") ?? [];
     }
+
+    public async Task<string> ShortenLink(string longUrl, string comment)
+    {
+        var req = new CreateLinkRequest()
+        {
+            LongUrl = longUrl,
+            Comment = comment
+        };
+
+        var response = await http.PostAsJsonAsync("c", req);
+        return await response.Content.ReadAsStringAsync();
+    }
 }
 
 public interface ILinkService
 {
     Task<ShortenedLink[]> GetLinksAsync();
+    Task<string> ShortenLink(string longUrl, string comment);
 }
--- Qurlify/Service/Model/ShortenedLink.cs
+++ Qurlify/Service/Model/ShortenedLink.cs
@@ -1,7 +1,7 @@
 public class ShortenedLink
 {
     public string id { get; set; } = Guid.NewGuid().ToString();
-    public string longUrl { get; set; }
+    public string longUrl { get; set; } = "";
     public string link { get; set; } = "";
     public int HitCounter { get; set; }
     public DateTime DateCreated { get; private set; } = DateTime.Now;
